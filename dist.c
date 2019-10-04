13412e79396b822317b8079b07f8cd4774e3a64f
deepthi_it19 <deepthi.it19@bitsathy.ac.in>
2019-10-04 11:36:38 +0530

    third commit

---
+++ dist.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int x,y;
+    printf("\nsample input:\n");
+    printf("enter the train length...:");
+    scanf("%d",&x);
+    printf("enter the brige length...:");
+    scanf("%d",&y);
+    printf("sample output\ntoal distance travelled...:%d",x+y);
+    return 0;
+}
